95bc566d71c48dd2b8e29482cfeaa965a3989dd6
HangNT169 <hangnt169@fpt.edu.vn>
2026-01-07 16:36:56 +0700

    init project

---
+++ src/main/java/com/poly/demo/SOF2043_SOF204301_BL1_SP26.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.poly.demo;
+
+/**
+ *
+ * @author hangnt
+ */
+public class SOF2043_SOF204301_BL1_SP26 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
